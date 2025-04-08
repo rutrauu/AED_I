3f31c7887197edef54fa7f2fae4963241ee93065
Artur <artur.raguse21@gmail.com>
2025-04-07 21:35:03 -0300

    aula 4

--- Aula04/TelaCarro.py
+++ Aula04/TelaCarro.py
@@ -2,10 +2,12 @@ import sys
 from PyQt5.QtWidgets import *
 
 from TelaVeiculo import TelaVeiculo
+from Carro import Carro
 
 class TelaCarro(TelaVeiculo):
     def __init__(self, titulo = "Tela de Carro"):
         super().__init__(titulo)
+        self.setGeometry(1300, 600, 300, 300)
 
     def definirLayout(self):
         super().definirLayout()
@@ -13,3 +15,16 @@ class TelaCarro(TelaVeiculo):
         self.txtPortas = QLineEdit(self)
         self.layout.addWidget(self.lblPortas)
         self.layout.addWidget(self.txtPortas)
+
+    def salvar(self):
+        modelo = self.txtModelo.text()
+        ano = self.txtAno.text()
+        if (ano != ""):
+            ano = int(ano)
+        portas = self.txtPortas.text()
+        if portas != "":
+            portas = int (portas)
+        carro = Carro(modelo, ano, portas)
+        QMessageBox.information(self, "Carro salvo!" + str(carro))
+
+        
